4c5812bc3afd3fe49d49621e67b82c3e96694c30
Wonwoo Seo <wonwooseo@hotmail.com>
2025-11-28 01:41:34 +0900

    card heading text size

--- app/components/base/Heading.vue
+++ app/components/base/Heading.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mb-2 p-3 flex items-center justify-between bg-white border-b border-gray-200">
     <div class="flex items-center gap-2">
-      <span class="font-bold text-slate-700">{{ heading }}</span>
+      <span class="font-bold text-base text-slate-700">{{ heading }}</span>
     </div>
   </div>
   <!-- inject content here -->
